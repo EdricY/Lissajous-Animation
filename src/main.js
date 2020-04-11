a15d3386505bdb211b31f0e6f1e83ed6d4ba4ee8
EdricY <yu.edric@gmail.com>
2020-04-11 13:21:23 -0500

    inversion! don't store ref to canvas, just use ctx.canvas beginning logic extract subtract function click event for debug

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import Point from "./point";
 import MathHelp from "./mathhelp";
 
+// delete window.OffscreenCanvas;
+
 if (!window.OffscreenCanvas) {
   window.OffscreenCanvas = class OffscreenCanvas {
     constructor(width, height) {
@@ -12,103 +14,106 @@ if (!window.OffscreenCanvas) {
   };
 }
 
+let inverted = true;
+
 let tickDur = 16;
 let lastTick = 0;
-
 export default class LissajousAnimator {
   constructor(canvas, options) {
-    this.canvas = canvas.transferControlToOffscreen();
-    this.canvas.width = canvas.clientWidth;
-    this.canvas.height = canvas.clientHeight;
-    this.ctx = this.canvas.getContext("2d");
-    this.textCanvas = new OffscreenCanvas(this.canvas.width, this.canvas.height);
-    this.textCtx = this.textCanvas.getContext("2d");
-    this.textCtx.font = "500px monospace"//Math.round(this.canvas.height/2) + "px monospace";
-    console.log(this.textCtx.font)
+    let osCanvas = canvas.transferControlToOffscreen();
+    this.w = canvas.clientWidth
+    this.h = canvas.clientHeight;
+    let { w, h } = this;
+    osCanvas.width = canvas.clientWidth;
+    osCanvas.height = canvas.clientHeight;
+    canvas.style.backgroundColor = "#000";
+
+    this.ctx = osCanvas.getContext("2d");
+    let textCanvas = new OffscreenCanvas(w, h);
+    this.textCtx = textCanvas.getContext("2d");
+    this.textCtx.font = "500px monospace"//Math.round(h/2) + "px monospace";
     this.textCtx.textAlign = "center";
     this.textCtx.textBaseline = "middle";
     this.textCtx.fillStyle = "red";
+    console.log(this.textCtx.font)
 
-    this.crossCanvas = new OffscreenCanvas(this.canvas.width, this.canvas.height);
-    this.crossCtx = this.crossCanvas.getContext("2d");
+    let crossCanvas = new OffscreenCanvas(w, h);
+    this.crossCtx = crossCanvas.getContext("2d");
 
-    this.maskCanvas = new OffscreenCanvas(this.canvas.width, this.canvas.height);
-    this.maskCtx = this.maskCanvas.getContext("2d");
+    let maskCanvas = new OffscreenCanvas(w, h);
+    this.maskCtx = maskCanvas.getContext("2d");
 
     this.head = new Point();
     this.tail = new Point();
+    
     this.a = 22//MathHelp.randInt(5, 30);
     this.b = 21//MathHelp.randInt(5, 30);
     this.playAnimation()
     this.count = 0;
+
+    //for debug
+    canvas.addEventListener("click", e => {
+      logPixel(this.ctx, e.clientX, e.clientY)
+    })
   }
 
   playAnimation() {
     let t = performance.now();
-    let w = this.canvas.width;
-    let h = this.canvas.height;
-
-    this.head.x = (w/2) + (w/2.3)*Math.sin(this.b*t/10000);
-    this.head.y = (h/2) + (h/2.3)*Math.cos(this.a*t/10000);
-    this.tail.x = this.head.x;
-    this.tail.y = this.head.y;
+    let { w, h } = this;
+    this.beginning = true;
 
     requestAnimationFrame(t => this.animate(t));
   }
 
   animate(t) {
-    if (t - lastTick < tickDur) {
+    let { a, b, w, h, ctx, textCtx, crossCtx, maskCtx } = this
+
+    if (this.beginning) {
+      this.head.x = (w/2) + (w/2.3)*Math.sin(b*t/5000);
+      this.head.y = (h/2) + (h/2.3)*Math.cos(a*t/5000);
+      this.tail.x = this.head.x;
+      this.tail.y = this.head.y;
+      this.beginning = false;
+      requestAnimationFrame(t => this.animate(t));
+      return;
+    }
+
+    if (t - lastTick < tickDur || this.beginning) {
       requestAnimationFrame(t => this.animate(t));
       return;
     }
     this.count++;
 
     lastTick = t;
-    let w = this.canvas.width;
-    let h = this.canvas.height;
-    let { a, b, head, tail, ctx, textCtx, crossCtx, maskCtx } = this
-    window.ctx = ctx;
-    tail.x = head.x;
-    tail.y = head.y;
-    head.x = (w/2) + (w/2.3)*Math.sin(b*t/5000);
-    head.y = (h/2) + (h/2.3)*Math.cos(a*t/5000);
-
-    ctx.globalCompositeOperation = "source-over";
-    ctx.fillStyle = 'rgba(0,0,0,.1)';
-    ctx.fillRect(0, 0, w, h);
+    // window.ctx = ctx;
+    this.tail.x = this.head.x;
+    this.tail.y = this.head.y;
+    this.head.x = (w/2) + (w/2.3)*Math.sin(b*t/5000);
+    this.head.y = (h/2) + (h/2.3)*Math.cos(a*t/5000);
     
     textCtx.clearRect(0, 0, w, h);
+    textCtx.globalCompositeOperation = "source-over";
+    if (inverted) {
+      textCtx.fillStyle = "white";
+      textCtx.fillRect(0,0,w,h)
+      textCtx.globalCompositeOperation = "destination-out";
+    }
     textCtx.fillText(getCurrentTimeString(), w/2, h/2);
 
     crossCtx.globalCompositeOperation = "source-over";
     this.drawHeadTailLine(crossCtx);
     crossCtx.globalCompositeOperation = "destination-in";
-    crossCtx.drawImage(this.textCanvas, 0, 0);
+    crossCtx.drawImage(textCtx.canvas, 0, 0);
 
-    maskCtx.globalCompositeOperation = "source-over";
-    maskCtx.fillStyle = '#000100';
-    maskCtx.fillRect(0, 0, w, h);
-    maskCtx.globalCompositeOperation = "destination-in";
-    maskCtx.drawImage(this.crossCanvas, 0, 0);
-    maskCtx.globalCompositeOperation = "difference";
-    maskCtx.drawImage(this.crossCanvas, 0, 0);
+    subtract(crossCtx, "#000001", maskCtx);
 
     crossCtx.globalCompositeOperation = "copy";
-    crossCtx.drawImage(this.maskCanvas, 0, 0)
+    crossCtx.drawImage(maskCtx.canvas, 0, 0)
     
-    this.ctx.drawImage(this.crossCanvas, 0, 0);
+    this.ctx.drawImage(crossCtx.canvas, 0, 0);
 
-
-    maskCtx.globalCompositeOperation = "source-over";
-    maskCtx.fillStyle = '#000100';
-    maskCtx.fillRect(0, 0, w, h);
-    maskCtx.globalCompositeOperation = "destination-in";
-    maskCtx.drawImage(this.canvas, 0, 0);
-    maskCtx.globalCompositeOperation = "difference";
-    maskCtx.drawImage(this.canvas, 0, 0);
-    
     ctx.globalCompositeOperation = "copy";
-    ctx.drawImage(this.maskCanvas, 0, 0);
+    ctx.drawImage(maskCtx.canvas, 0, 0);
     
     ctx.globalCompositeOperation = "source-over";
     this.drawHeadTailLine(ctx);
@@ -118,7 +123,7 @@ export default class LissajousAnimator {
 
   drawHeadTailLine(ctx) {
     ctx.beginPath();
-    ctx.strokeStyle = '#0F0';
+    ctx.strokeStyle = '#00F';
     ctx.lineWidth = 100;
     ctx.lineJoin = "round"
     ctx.lineCap = "round"
@@ -128,11 +133,35 @@ export default class LissajousAnimator {
   }
 }
 
+function subtract(ctx, subColor, tempCtx) {
+  let w = ctx.canvas.width;
+  let h = ctx.canvas.height;
+  if (!tempCtx) {
+    let tempCanvas = new OffscreenCanvas(w, h);
+    tempCtx = tempCanvas.getContext("2d");
+  }
+  tempCtx.globalCompositeOperation = "source-over";
+  tempCtx.fillStyle = subColor;
+  tempCtx.fillRect(0, 0, w, h);
+  tempCtx.globalCompositeOperation = "destination-in";
+  tempCtx.drawImage(ctx.canvas, 0, 0);
+  tempCtx.globalCompositeOperation = "difference";
+  tempCtx.drawImage(ctx.canvas, 0, 0);
+
+  ctx.drawImage(tempCtx.canvas, 0, 0);
+}
+
 function getCurrentTimeString() {
   let now = new Date();
   let hh = now.getHours().toString().padStart(2, '0');
   let mm = now.getMinutes().toString().padStart(2, '0')
+  // let mm = now.getSeconds().toString().padStart(2, '0')
   return `${hh}:${mm}`;
+}
+
+function logPixel(ctx, x, y) {
+  let arr = ctx.getImageData(x, y, 1, 1).data;
+  console.log(arr);
 }
 
 if (process.browser) window.LissajousAnimator = LissajousAnimator;
